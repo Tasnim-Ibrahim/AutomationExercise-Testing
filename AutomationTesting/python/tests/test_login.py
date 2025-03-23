945204b4573e340eb55a2f416ad65619e276817c
Tasnim Ibrahim Hassan <149611972+Tasnim-Ibrahim@users.noreply.github.com>
2025-03-23 14:18:28 +0200

    Update test_login.py

--- AutomationTesting/python/tests/test_login.py
+++ AutomationTesting/python/tests/test_login.py
@@ -1,19 +1,17 @@
+# test_login.py
 import pytest
 from pages.login_page import LoginPage
 
-@pytest.mark.usefixtures("setup")
 class TestLogin:
 
     def test_valid_login(self, driver):
         login_page = LoginPage(driver)
         login_page.go_to_login()
         login_page.login("dummytester789@mail.com", "Test@1234")
-
         assert "Logged in as" in driver.page_source
 
     def test_invalid_login(self, driver):
         login_page = LoginPage(driver)
         login_page.go_to_login()
         login_page.login("invaliduser@example.com", "wrongpassword")
-
         assert "Your email or password is incorrect!" in driver.page_source
