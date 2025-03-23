85bbf698118b41c17a84d673ff26e1b103ce5b2e
Tasnim Ibrahim Hassan <149611972+Tasnim-Ibrahim@users.noreply.github.com>
2025-03-23 14:16:57 +0200

    Update conftest.py

--- AutomationTesting/python/conftest.py
+++ AutomationTesting/python/conftest.py
@@ -1,10 +1,12 @@
+# 📂 AutomationTesting/python/conftest.py
+
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
 @pytest.fixture(scope="function")
-def setup():
+def driver():
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
     driver.maximize_window()
     driver.get("https://automationexercise.com")
