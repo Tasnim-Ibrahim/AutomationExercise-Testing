c18a33fc9c80c2f23849d979254f6804e3c1c886
Tasnim Ibrahim Hassan <149611972+Tasnim-Ibrahim@users.noreply.github.com>
2025-03-23 14:53:54 +0200

    Create signup_page.py

---
+++ AutomationTesting/python/pages/signup_page.py
@@ -0,0 +1,44 @@
+# 📂 automation/python/pages/signup_page.py
+import random
+import string
+from selenium.webdriver.common.by import By
+
+class SignupPage:
+    def __init__(self, driver):
+        self.driver = driver
+        self.signup_login_link = (By.LINK_TEXT, "Signup / Login")
+        self.name_field = (By.NAME, "name")
+        self.email_field = (By.XPATH, "//input[@data-qa='signup-email']")
+        self.signup_button = (By.XPATH, "//button[@data-qa='signup-button']")
+        self.password_field = (By.ID, "password")
+        self.first_name = (By.ID, "first_name")
+        self.last_name = (By.ID, "last_name")
+        self.address = (By.ID, "address1")
+        self.state = (By.ID, "state")
+        self.city = (By.ID, "city")
+        self.zipcode = (By.ID, "zipcode")
+        self.mobile = (By.ID, "mobile_number")
+        self.create_account_btn = (By.XPATH, "//button[@data-qa='create-account']")
+
+    def go_to_signup(self):
+        self.driver.find_element(*self.signup_login_link).click()
+
+    def generate_random_email(self):
+        prefix = ''.join(random.choices(string.ascii_lowercase + string.digits, k=6))
+        return f"testuser_{prefix}@example.com"
+
+    def fill_signup_form(self, name, email, password):
+        self.driver.find_element(*self.name_field).send_keys(name)
+        self.driver.find_element(*self.email_field).send_keys(email)
+        self.driver.find_element(*self.signup_button).click()
+
+        self.driver.find_element(*self.password_field).send_keys(password)
+        self.driver.find_element(*self.first_name).send_keys("Test")
+        self.driver.find_element(*self.last_name).send_keys("User")
+        self.driver.find_element(*self.address).send_keys("123 Test Lane")
+        self.driver.find_element(*self.state).send_keys("Test State")
+        self.driver.find_element(*self.city).send_keys("Test City")
+        self.driver.find_element(*self.zipcode).send_keys("12345")
+        self.driver.find_element(*self.mobile).send_keys("1234567890")
+
+        self.driver.find_element(*self.create_account_btn).click()
