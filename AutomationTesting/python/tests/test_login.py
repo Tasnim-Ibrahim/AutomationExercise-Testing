fcb5b0c873cc7b285a576849edb861928f9cf7a5
Tasnim Ibrahim Hassan <149611972+Tasnim-Ibrahim@users.noreply.github.com>
2025-03-23 14:54:51 +0200

    Update test_login.py

--- AutomationTesting/python/tests/test_login.py
+++ AutomationTesting/python/tests/test_login.py
@@ -1,6 +1,7 @@
-# test_login.py
+# 📂 automation/python/tests/test_login.py
 import pytest
 from pages.login_page import LoginPage
+from pages.signup_page import SignupPage
 
 class TestLogin:
 
@@ -15,3 +16,10 @@ class TestLogin:
         login_page.go_to_login()
         login_page.login("invaliduser@example.com", "wrongpassword")
         assert "Your email or password is incorrect!" in driver.page_source
+
+    def test_signup(self, driver):
+        signup_page = SignupPage(driver)
+        signup_page.go_to_signup()
+        email = signup_page.generate_random_email()
+        signup_page.fill_signup_form("Automation Tester", email, "Test@1234")
+        assert "ACCOUNT CREATED!" in driver.page_source
